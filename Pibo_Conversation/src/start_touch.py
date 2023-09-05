5c893056fc203265bea7d3544850dc2e62936c48
daaa02 <dyk98498@gmail.com>
2023-09-05 14:09:56 +0900

    Update_230905

--- Pibo_Conversation/src/start_touch.py
+++ Pibo_Conversation/src/start_touch.py
@@ -55,34 +55,32 @@ while True:
                 device.eye_off(); time.sleep(0.5); 
                 device.eye_on(255,255,255); time.sleep(0.5) 
             
-            # answer = cm.responses_proc(re_bhv="do_waiting_A", re_q="나한테 안녕이라고 말해줄래?")
-            # print(answer)
+            answer = cm.responses_proc(re_bhv="do_waiting_A", re_q="나한테 안녕이라고 말해줄래?")
+            print(answer)
             
-            # # 키워드 단순화
-            # if "소개" in answer[0][1] or "반가" in answer[0][1]:
-            #     # os.system('python3 /home/pi/Pibo_Package_13/Pibo_Conversation/src/greeting.py')
-            #     break
+            # 키워드 단순화
+            if "소개" in answer[0][1] or "반가" in answer[0][1]:
+                # os.system('python3 /home/pi/Pibo_Package_13/Pibo_Conversation/src/greeting.py')
+                break
             
-            # if "헤어" in answer[0][1]:
-            #     # os.system('python3 /home/pi/Pibo_Package_13/Pibo_Conversation/src/goodbye.py') 
-            #     break
+            if "헤어" in answer[0][1]:
+                # os.system('python3 /home/pi/Pibo_Package_13/Pibo_Conversation/src/goodbye.py') 
+                break
             
-            # else:    
-            #     cm.tts(bhv="do_question_L", string=f"새로운 활동을 시작해볼까?")
-            #     answer = cm.responses_proc(re_bhv="do_question_L", re_q="오늘의 활동을 시작해볼까?",
-            #                             pos_bhv="do_stop", pos=f"시작하자~", 
-            #                             neg_bhv="do_stop", neg=f"알겠어. 나랑 놀고 싶다면 다시 쓰다듬어줘")
+            else:    
+                cm.tts(bhv="do_question_L", string=f"새로운 활동을 시작해볼까?")
+                answer = cm.responses_proc(re_bhv="do_question_L", re_q="오늘의 활동을 시작해볼까?",
+                                        pos_bhv="do_stop", pos=f"시작하자~", 
+                                        neg_bhv="do_stop", neg=f"알겠어. 나랑 놀고 싶다면 다시 쓰다듬어줘")
                 
-            #     time.sleep(1)
-            #     if answer[0][0] == 'positive' or answer[0][0] == 'action':
-            #         cm.tts(bhv="do_stop", string=f"잠시만 기다려줘. 재미있는 활동을 생각해볼께!")
-            #         break
+                time.sleep(1)
+                if answer[0][0] == 'positive' or answer[0][0] == 'action':
+                    cm.tts(bhv="do_stop", string=f"잠시만 기다려줘. 재미있는 활동을 생각해볼께!")
+                    break
                     
-            #     else:
-            #         oled.clear()
-            #         continue
-            break
+                else:
+                    oled.clear()
+                    continue
             
-# os.system('python3 /home/pi/Pibo_Package_13/Pibo_Conversation/src/schedule_run.py')
-os.system('python3 /home/pi/Pibo_Package_13/Pibo_Conversation/src/goodbye.py')
+os.system('python3 /home/pi/Pibo_Package_13/Pibo_Conversation/src/schedule_run.py')
     
